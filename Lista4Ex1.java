04cc9e8ae51b62c567b07bee64174303a2075240
leticiampetris <leticiampetris@gmail.com>
2018-05-03 11:25:50 -0300

    Inicialização do repositório

---
+++ Lista4Ex1.java
@@ -0,0 +1,44 @@
+public class Lista4Ex1{
+
+    public static void main (String[]args){
+
+    int[] numero = new int [16];
+    numero[0] = 2;
+    numero[1] = 50;
+    numero[2] = 5;
+    numero[3] = 4;
+    numero[4] = 6;
+    numero[5] = 8;
+    numero[6] = 12;
+    numero[7] = 46;
+    numero[8] = 4;
+    numero[9] = 7;
+    numero[10] = 6;
+    numero[11] = 80;
+    numero[12] = 6;
+    numero[13] = 10;
+    numero[14] = 7;
+    numero[15] = 63;
+        
+        int soma = numero[0] +
+                   numero[1] +
+                   numero[2] +
+                   numero[3] +
+                   numero[4] +
+                   numero[5] +
+                   numero[6] +
+                   numero[7] +
+                   numero[8] +
+                   numero[9] +
+                   numero[10] +
+                   numero[11] +
+                   numero[12] +
+                   numero[13] +
+                   numero[14] +
+                   numero[15];
+
+    System.out.print("Soma: " + soma);
+
+
+    }
+}
---
+++ Lista4Ex2.java
@@ -0,0 +1,30 @@
+public class Lista4Ex2{
+
+    public static void main (String[]args){
+
+    String[] nome = new String [10];
+    nome[0] = "A";
+    nome[1] = "B";
+    nome[2] = "C";
+    nome[3] = "D";
+    nome[4] = "E";
+    nome[5] = "F";
+    nome[6] = "G";
+    nome[7] = "H";
+    nome[8] = "I";
+    nome[9] = "J";         
+    
+    System.out.print(nome[0] +
+             "\n" + nome[1] +
+             "\n" + nome[2] +
+             "\n" + nome[3] +
+             "\n" + nome[4] +
+             "\n" + nome[5] +
+             "\n" + nome[6] +
+             "\n" + nome[7] +
+             "\n" + nome[8] +
+             "\n" + nome[9]);
+
+
+    }
+}
